847093abeb974bff52223edd47d0bc4ed4fd40d6
theoliver264 <31642690+theoliver264@users.noreply.github.com>
2019-01-16 13:52:08 -0600

    Initial Commit

---
+++ main.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function () {
+    let elems = document.querySelectorAll('.sidenav');
+    let instances = M.Sidenav.init(elems);
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    let elems = document.querySelectorAll('.carousel');
+    let instances = M.Carousel.init(elems, {
+        fullWidth: true,
+        indicators: true
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    let elems = document.querySelectorAll('.parallax');
+    let instances = M.Parallax.init(elems);
+
+    
+});
